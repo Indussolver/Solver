fdc0b01be69ea8dd7603c5c58f91d06424d13c7b
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 17:22:49 +0530

    Update alarm-storage.js

--- js/alarm/alarm-storage.js
+++ js/alarm/alarm-storage.js
@@ -1,13 +1,1 @@
-function saveAlarms(){
-    localStorage.setItem("alarms", JSON.stringify(alarms));
-}
 
-function loadAlarms(){
-    const data = localStorage.getItem("alarms");
-
-    if(data){
-        alarms = JSON.parse(data);
-    }
-}
-
-loadAlarms();
