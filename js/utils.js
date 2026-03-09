54d823ec3b8078a89566189fe53baaa4f24e1f0d
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 17:42:21 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,30 @@
+const Utils = {
+
+DOM:{
+on(element,event,callback){
+if(!element) return;
+element.addEventListener(event,callback);
+}
+},
+
+Storage:{
+get(key){
+try{
+return JSON.parse(localStorage.getItem(key));
+}catch{
+return localStorage.getItem(key);
+}
+},
+
+set(key,value){
+localStorage.setItem(key,JSON.stringify(value));
+},
+
+remove(key){
+localStorage.removeItem(key);
+}
+
+}
+
+};
 
