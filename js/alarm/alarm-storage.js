2ba917d43575db9a5cc975e7f87e0fb0c01d81c2
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 18:57:31 +0530

    Update alarm-storage.js

--- js/alarm/alarm-storage.js
+++ js/alarm/alarm-storage.js
@@ -1,1 +1,13 @@
+// Local storage for saving alarms
 
+export function saveAlarm(time){
+
+localStorage.setItem("solver_alarm",time);
+
+}
+
+export function loadAlarm(){
+
+return localStorage.getItem("solver_alarm");
+
+}
