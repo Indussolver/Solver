71293343761843234e3cec35011a6a7b078ce574
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 18:57:46 +0530

    Update alarm-ui.js

--- js/alarm/alarm-ui.js
+++ js/alarm/alarm-ui.js
@@ -1,1 +1,40 @@
+import { setAlarm, startAlarmChecker } from "./core.js";
 
+const alarmToggle = document.getElementById("alarmToggle");
+const alarmDate = document.getElementById("alarmDate");
+const alarmTime = document.getElementById("alarmTime");
+const setAlarmBtn = document.getElementById("setAlarm");
+const status = document.getElementById("alarmStatus");
+
+function updateStatus(message){
+
+status.innerText = message;
+
+}
+
+setAlarmBtn.addEventListener("click",()=>{
+
+if(!alarmToggle.checked){
+
+updateStatus("Turn on the alarm first");
+
+return;
+
+}
+
+const date = alarmDate.value;
+const time = alarmTime.value;
+
+if(!date || !time){
+
+updateStatus("Select date and time");
+
+return;
+
+}
+
+setAlarm(date,time,updateStatus);
+
+});
+
+startAlarmChecker(updateStatus);
