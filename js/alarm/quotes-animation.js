221292426bf263b05bab5a1686b8071a439b3833
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 20:12:21 +0530

    Update quotes-animation.js

--- js/alarm/quotes-animation.js
+++ js/alarm/quotes-animation.js
@@ -1,50 +1,15 @@
-const cards = document.querySelectorAll(".quote-card")
+document.addEventListener("DOMContentLoaded", function(){
 
-function revealCards(){
-
-const trigger = window.innerHeight * 0.85
-
-cards.forEach(card => {
-
-const top = card.getBoundingClientRect().top
-
-if(top < trigger){
-
-card.style.opacity = "1"
-card.style.transform = "translateY(0)"
-
-}
-
-})
-
-}
-
-window.addEventListener("scroll",revealCards)
 const quotes = document.querySelectorAll(".big-quote");
 
-function revealQuotes() {
+quotes.forEach((quote, index) => {
 
-const triggerPoint = window.innerHeight * 0.85;
+setTimeout(() => {
 
-quotes.forEach((quote) => {
+quote.classList.add("show");
 
-const quoteTop = quote.getBoundingClientRect().top;
-
-if (quoteTop < triggerPoint) {
-
-quote.style.opacity = "1";
-quote.style.transform = "translateY(0)";
-
-}
+}, index * 400);
 
 });
 
-}
-
-/* run on scroll */
-
-window.addEventListener("scroll", revealQuotes);
-
-/* run once when page loads */
-
-window.addEventListener("load", revealQuotes);
+});
