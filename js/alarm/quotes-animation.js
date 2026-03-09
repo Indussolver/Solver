b475437f3fad98dcb2e27cfaf1489362eb2300ea
Indussolver <indusplusbusiness@gmail.com>
2026-03-09 19:53:52 +0530

    Update quotes-animation.js

--- js/alarm/quotes-animation.js
+++ js/alarm/quotes-animation.js
@@ -20,3 +20,25 @@ card.style.transform = "translateY(0)"
 }
 
 window.addEventListener("scroll",revealCards)
+const bigQuotes = document.querySelectorAll(".big-quote")
+
+function revealBigQuotes(){
+
+const trigger = window.innerHeight * 0.85
+
+bigQuotes.forEach(q => {
+
+const top = q.getBoundingClientRect().top
+
+if(top < trigger){
+
+q.style.opacity = "1"
+q.style.transform = "translateY(0)"
+
+}
+
+})
+
+}
+
+window.addEventListener("scroll", revealBigQuotes)
